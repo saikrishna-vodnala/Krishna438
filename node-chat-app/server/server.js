0a71b391e18a28c65e6ae4cee16aa82a32f0930d
saikrishna-vodnala <saikrishna.vodnala@walkingtree.tech>
2018-04-04 18:22:47 +0530

    custom events commited

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname,'/../public');
+const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
@@ -14,12 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New User Connected');
 
+	socket.emit('newMessage', {
+		from: 'krishna@gmail.com',
+		text: 'Hii sai',
+		createdAt: 43234
+	});
+
+	socket.on('createMessage', (msg) => {
+		console.log('Create Message', msg);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
 	});
 });
-
-
 
 server.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
