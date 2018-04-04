7335cac456e2e04db25c1cbf8a8a8a9ce2924a88
saikrishna-vodnala <saikrishna.vodnala@walkingtree.tech>
2018-04-04 16:56:40 +0530

    Adding Connect & Disconnect Events

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,13 +1,26 @@
 const path = require('path'); //built-in module used for path joining
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'/../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+io.on('connection', (socket) => {
+	console.log('New User Connected');
+
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
+});
+
+
+
+server.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
